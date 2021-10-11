62a4c41694bc2e0323bd28574345dea3664012e2
Patrick <pat14trani@gmail.com>
2021-10-11 15:27:25 -0700

    Functionality Structure
    Now that we have learned how to detect what projectile strikes us, we can begin to implement the item logic. What has been changed it the TakeDamage hook to comment out debug code and layout the logic flow for the item.

--- RHR/RedHotRuby/Items/RedHotRuby.cs
+++ RHR/RedHotRuby/Items/RedHotRuby.cs
@@ -15,19 +15,21 @@ namespace RedHotRuby.Items
 
         public override string ItemPickupDesc => "A flaming ruby surrounds your body with warmth";
 
-        public override string ItemFullDescription => $"When you take fire damage,  <style=cIsUtility>{MainDamageReduced}</style> <style=cStack>+{AdditionalDamageReductionPerStack}</style>";
+        public override string ItemFullDescription => $"This Item absorbs Lumerian Fireballs and stores them to be fired out at enemies.";
 
         public override string ItemLore => "These flaming rubies are mined from the depths of hell. Their horns grow out of them and if picked up they shockingly don't burn you but rather they help the user's body become acustom to scalding temperatures.";
 
-        public override ItemTier Tier => ItemTier.Tier1;
+        public override ItemTier Tier => ItemTier.Tier2;
 
         public override GameObject ItemModel => MainAssets.LoadAsset<GameObject>("RedHotRuby.prefab");
 
         public override Sprite ItemIcon => MainAssets.LoadAsset<Sprite>("RHRIcon.png");
 
         //Following are the variables we'd like to use
-        public float MainDamageReduced = 4;
-        public float AdditionalDamageReductionPerStack = 4;
+        public float FireBallCount = 10;
+        public float AdditionalFireballsPerStack = 5;
+        public float charge = 0;
+        public float chargeDamage;
 
         public static GameObject ItemBodyModelPrefab;
 
@@ -41,8 +43,8 @@ namespace RedHotRuby.Items
 
         public override void CreateConfig(ConfigFile config)
         {
-            MainDamageReduced = config.Bind<float>("Item: " + ItemName, "Main Damage Reduction", 10f, "How much damage reduction should the item do?").Value;
-            AdditionalDamageReductionPerStack = config.Bind<float>("Item: " + ItemName, "Increase to Damage Reduction per stack", 6f, "How much should the reduction increase by per additional stack?").Value;
+            FireBallCount = config.Bind<float>("Item: " + ItemName, "Starting amount of fireballs to let loose", 10f, "How many fireballs should be fired off?").Value;
+            AdditionalFireballsPerStack = config.Bind<float>("Item: " + ItemName, "Increase the amount of fireballs fired off per stack", 5f, "How many fireballs should be fired off?").Value;
         }
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
@@ -60,7 +62,7 @@ namespace RedHotRuby.Items
                     childName = "Chest",
                     localPos = new Vector3(1f, .6f, -.2f),
                     localAngles = new Vector3(0,0,0),
-                    localScale = new Vector3(1,1,1)
+                    localScale = new Vector3(20,20,20)
                 }
             });
             return rules;
@@ -68,17 +70,34 @@ namespace RedHotRuby.Items
 
         public override void Hooks()
         {
-            On.RoR2.HealthComponent.TakeDamage += GetDamage;
+            On.RoR2.HealthComponent.TakeDamage += TakeDamage;
         }
 
-        private void GetDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, RoR2.HealthComponent self, RoR2.DamageInfo damageInfo)
+        private void TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, RoR2.HealthComponent self, RoR2.DamageInfo damageInfo)
         {
             if (!damageInfo.rejected || damageInfo == null)
             {
                 var inventoryCount = GetCount(self.body);
-                if (damageInfo.inflictor) && inventoryCount > 0)
+                if (inventoryCount > 0)
                 {
-                    Chat.AddMessage("Hit by fire damage");
+                    //Debug
+                    //Chat.AddMessage($"Damage Type: {damageInfo.damageType}");
+                    //Chat.AddMessage($"Inflictor : {damageInfo.inflictor}");
+                    Chat.AddMessage($"Inflictor : {damageInfo.inflictor.tag}");
+                    Chat.AddMessage($"Inflictor : {damageInfo.inflictor.name}");
+
+                    if (damageInfo.inflictor.name == "Fireball(Clone)")
+                    {
+                        //particle effect?
+                        Chat.AddMessage("Item Worked");
+                        damageInfo.damage = 0f;
+                        charge += 1;
+                        if(charge >= 10)
+                        {
+                            Chat.AddMessage("Boom");
+                            //blast balls
+                        }
+                    }
                 }
             }
 
